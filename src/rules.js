cc922736b7dbd61e1131209503c4c6672eddab28
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-08-11 08:24:58 +0200

    Update configs and move to npm7
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/rules.js
+++ src/rules.js
@@ -3,7 +3,7 @@
  *
  * @author John Molakvoæ <skjnldsv@protonmail.com>
  *
- * @license GNU AGPL version 3 or any later version
+ * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -23,8 +23,8 @@
 /**
  * Generate an upload limit message based on the provided max size
  *
- * @param {int} size The upload limit size
- * @returns {string}
+ * @param {number} size The upload limit size
+ * @return {string}
  */
 const uploadLimit = size => t(
 	'browser_warning',
